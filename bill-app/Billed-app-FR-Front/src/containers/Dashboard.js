1fcb757282c67cf0ac3e66dd2e17e145814a58fe
A_Larose <larose.alain@gmail.com>
2022-11-07 11:54:22 +0100

    Résolution du Bug Hunt Dashboard

--- bill-app/Billed-app-FR-Front/src/containers/Dashboard.js
+++ bill-app/Billed-app-FR-Front/src/containers/Dashboard.js
@@ -133,10 +133,16 @@ export default class {
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
+    const statusBillsList = filteredBills(bills, getStatus(this.index)) 
     if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)'})
       $(`#status-bills-container${this.index}`)
-        .html(cards(filteredBills(bills, getStatus(this.index))))
+        // .html(cards(filteredBills(bills, getStatus(this.index))))
+        .html(cards(statusBillsList))
+      statusBillsList
+        .forEach(bill => {
+          $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+        })
       this.counter ++
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)'})
@@ -145,9 +151,10 @@ export default class {
       this.counter ++
     }
 
-    bills.forEach(bill => {
-      $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
-    })
+    // bills.forEach(bill => {
+    //  .forEach(bill => {
+    //    $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
+    //  })
 
     return bills
 
