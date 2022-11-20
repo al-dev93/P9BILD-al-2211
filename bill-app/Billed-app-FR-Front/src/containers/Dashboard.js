db790a653de00d1860e88dad7d4474f9ce120247
A_Larose <larose.alain@gmail.com>
2022-11-20 03:29:41 +0100

    Taille de l'image dans la modale Justificatif dans la modale du profil administrateur

--- bill-app/Billed-app-FR-Front/src/containers/Dashboard.js
+++ bill-app/Billed-app-FR-Front/src/containers/Dashboard.js
@@ -80,7 +80,7 @@ export default class {
 
   handleClickIconEye = () => {
     const billUrl = $('#icon-eye-d').attr("data-bill-url")
-    const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.8)
+    const imgWidth = Math.floor($('#modaleFileAdmin1').width() * 0.55)
     $('#modaleFileAdmin1').find(".modal-body").html(`<div style='text-align: center;'><img width=${imgWidth} src=${billUrl} alt="Bill"/></div>`)
     if (typeof $('#modaleFileAdmin1').modal === 'function') $('#modaleFileAdmin1').modal('show')
   }
@@ -152,7 +152,6 @@ export default class {
     }
 
     // bills.forEach(bill => {
-    //  .forEach(bill => {
     //    $(`#open-bill${bill.id}`).click((e) => this.handleEditTicket(e, bill, bills))
     //  })
 
